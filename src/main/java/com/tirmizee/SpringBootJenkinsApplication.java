5eba27e03b891e76d16dae920592d7243c39c910
Pratya Yeekhaday <outsource1842@setth.onmicrosoft.com>
2023-06-26 21:48:54 +0700

    update project

--- src/main/java/com/tirmizee/SpringBootJenkinsApplication.java
+++ src/main/java/com/tirmizee/SpringBootJenkinsApplication.java
@@ -2,12 +2,20 @@ package com.tirmizee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootJenkinsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJenkinsApplication.class, args);
+	}
+
+	@GetMapping("/test")
+	public String test() {
+		return "hello world";
 	}
 
 }
